62006586802a4bab351a7b5c311a82d9fa2b1a2e
Keshav Gupta <keshavkk191103@gmail.com>
2023-12-18 17:34:14 +0530

    first commit

---
+++ src/data/RatingColors.js
@@ -0,0 +1,44 @@
+// Assuming this is your original RankColors array
+export const RankColors = [
+  {
+    rank: "newbie",
+    color: "#808080",
+  },
+  {
+    rank: "pupil",
+    color: "#008000",
+  },
+  {
+    rank: "specialist",
+    color: "#00FFFF",
+  },
+  {
+    rank: "expert",
+    color: "#0000FF",
+  },
+  {
+    rank: "candidate master",
+    color: "#800080",
+  },
+  {
+    rank: "master",
+    color: "#FFFF00",
+  },
+  {
+    rank: "international master",
+    color: "#FFA500",
+  },
+  {
+    rank: "grandmaster",
+    color: "#FF0000",
+  },
+  {
+    rank: "international grandmaster",
+    color: "#CD5C5C",
+  },
+  {
+    rank: "legendary grandmaster",
+    color: "#FF69B4",
+  },
+];
+
